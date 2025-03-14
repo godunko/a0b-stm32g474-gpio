a662bf5e62c278b49d90c4897fbbe9e74d3637d4
Vadim Godunko <vgodunko@gmail.com>
2025-03-14 20:55:53 +0400

    Function for useful literals.

--- source/a0b-stm32g474-gpio.ads
+++ source/a0b-stm32g474-gpio.ads
@@ -1,5 +1,5 @@
 --
---  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--  Copyright (C) 2025, Vadim Godunko <vgodunko@gmail.com>
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
@@ -18,8 +18,20 @@ is
 
    subtype GPIO_Output_Mode is A0B.STM32.GPIO_Output_Mode;
 
+   function Push_Pull return GPIO_Output_Mode is (A0B.STM32.Push_Pull);
+   function Open_Drain return GPIO_Output_Mode is (A0B.STM32.Open_Drain);
+
    subtype GPIO_Output_Speed is A0B.STM32.GPIO_Output_Speed;
 
+   function Low return GPIO_Output_Speed is (A0B.STM32.Low);
+   function Medium return GPIO_Output_Speed is (A0B.STM32.Medium);
+   function High return GPIO_Output_Speed is (A0B.STM32.High);
+   function Very_High return GPIO_Output_Speed is (A0B.STM32.Very_High);
+
    subtype GPIO_Pull_Mode is A0B.STM32.GPIO_Pull_Mode;
+
+   function No return GPIO_Pull_Mode is (A0B.STM32.No);
+   function Pull_Up return GPIO_Pull_Mode is (A0B.STM32.Pull_Up);
+   function Pull_Down return GPIO_Pull_Mode is (A0B.STM32.Pull_Down);
 
 end A0B.STM32G474.GPIO;
